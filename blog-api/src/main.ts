58ad920c140dfd05912c4731d9c08cf2ce1f39e1
Danil Lystopad <danyl.lystopad@codeandcakes.com>
2024-11-12 12:46:28 +0200

    feat: hide link to front-end

--- blog-api/src/main.ts
+++ blog-api/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
